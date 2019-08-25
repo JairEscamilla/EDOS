390ae114337394905fb2e04a0f0f90a9fe46dc62
Jair Escamilla <hernandez_yo14@outlook.es>
2019-08-24 23:59:06 -0500

    Agregando nodo

--- code.c
+++ code.c
@@ -1,13 +1,20 @@
 #include <stdio.h> // Incluyendo bibliotecas
 #include <stdlib.h>
 
+// Definiendo estructura
+typedef struct defNodo{
+  float x, y;
+  struct defNodo* sig;
+}Nodo;
+
 // PROTOTIPOS DE FUNCIONES
 float f(float x, float y);
-
+void agregarNodo(float x, float y, Nodo** Inicio);
 // FUNCION PRINCIPAL
 int main(){
   int x0, y0, n;
   float h;
+  Nodo* Inicio = NULL;
   printf("Ingresar x0-> ");
   scanf("%d", &x0);
   printf("Ingresar y0-> ");
@@ -25,4 +32,21 @@ int main(){
 // Función de ecuacion diferencial
 float f(float x, float y){
   return x + y;
+}
+
+// Funcion que agrega un nodo a la lista dinamica
+void agregarNodo(float x, float y, Nodo** Inicio){
+  Nodo* temp = *Inicio;
+  Nodo* temp2 = (Nodo*)malloc(sizeof(Nodo));
+  temp2->x = x;
+  temp2->y = y;
+  temp2->sig = NULL;
+  if (temp == NULL) {
+    *Inicio = temp;
+  }else{
+    while (temp->sig != NULL) {
+      temp = temp->sig;
+    }
+    temp->sig = temp2;
+  }
 }
