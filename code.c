1a4e3d97dee1bf04679f8737b0399de65040d45c
Jair Escamilla <hernandez_yo14@outlook.es>
2019-08-24 23:41:17 -0500

    Comenzando a escribir el codigo

---
+++ code.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // Incluyendo bibliotecas
+#include <stdlib.h>
+
+// PROTOTIPOS DE FUNCIONES
+
+
+// FUNCION PRINCIPAL
+int main(){
+
+  return 0;
+}
+
+// DESARROLLANDO LAS FUNCIONES
