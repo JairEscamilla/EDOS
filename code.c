39291ef01d0467f69b8e2c612cfd79dafd1e8166
Jair Escamilla <hernandez_yo14@outlook.es>
2019-09-10 10:34:10 -0500

    Generando arreglos

--- code.c
+++ code.c
@@ -7,22 +7,36 @@
 // PROTOTIPOS DE FUNCIONES
 float f(float x, float y);
 void euler(float x0, float y0, int n, float h);
+float* generarArreglo(int cant);
+void liberarMemoria(float* arr);
 void plot();
 // FUNCION PRINCIPAL
 int main(){
-  int n;
-  float h, x0, y0;
-  printf("Ingresar x0-> ");
-  scanf("%f", &x0);
-  printf("Ingresar y0-> ");
-  scanf("%f", &y0);
+  int n, orden;
+  float h, x0, y0, *valoresNuevos, *valoresIniciales, *uT;
+  printf("Ingresar el orden de la EDO-> ");
+  scanf("%d", &orden);
   printf("Ingresar tamaño de salto (h)-> ");
   scanf("%f", &h);
   printf("Ingresar numero de pasos (n)-> ");
   scanf("%d", &n);
-  printf("Espere un momento mientras se calcula la solucion... \n");
-  euler(x0, y0, n, h);
-  printf("Ejecucion terminada... \n");
+  if(orden == 1){
+    printf("Ingresar x0-> ");
+    scanf("%f", &x0);
+    printf("Ingresar y0-> ");
+    scanf("%f", &y0);
+    printf("Espere un momento mientras se calcula la solucion... \n");
+    euler(x0, y0, n, h);
+    printf("Ejecucion terminada... \n");
+  }else{
+    valoresNuevos = generarArreglo(orden);
+    valoresIniciales = generarArreglo(orden);
+    uT = generarArreglo(orden);
+
+    liberarMemoria(valoresNuevos);
+    liberarMemoria(valoresIniciales);
+    liberarMemoria(uT);
+  }
   plot();
   return 0;
 }
@@ -54,6 +68,13 @@ void euler(float x0, float y0, int n, float h){
 
 //  plot exp(x)*(-x*exp(-x)-exp(-x)+2), "solucion.dat" with lines
 
+float* generarArreglo(int cant){
+  return (float*)malloc(cant*sizeof(float));
+}
+
+void liberarMemoria(float* arr){
+  free(arr);
+}
 
 // Función que gráfica el archivo de texto generado
 void plot(){
