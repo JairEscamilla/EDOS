f1ca22841cda1387adfa79e5d93c26e212b33c7f
Jair Escamilla <hernandez_yo14@outlook.es>
2019-09-10 10:23:23 -0500

    Eliminando listas dinamicas

--- code.c
+++ code.c
@@ -1,24 +1,17 @@
 #include <stdio.h> // Incluyendo bibliotecas
 #include <stdlib.h>
+#include <math.h>
 
-// Definiendo estructura
-typedef struct defNodo{
-  float x, y;
-  struct defNodo* sig;
-}Nodo;
+
 
 // PROTOTIPOS DE FUNCIONES
 float f(float x, float y);
-void agregarNodo(float x, float y, Nodo** Inicio);
-void euler(float x0, float y0, int n, float h, Nodo** Inicio);
-void guardarEnArchivo(Nodo* Inicio);
-void liberarMemoria(Nodo* Inicio);
+void euler(float x0, float y0, int n, float h);
 void plot();
 // FUNCION PRINCIPAL
 int main(){
   int n;
   float h, x0, y0;
-  Nodo* Inicio = NULL;
   printf("Ingresar x0-> ");
   scanf("%f", &x0);
   printf("Ingresar y0-> ");
@@ -28,10 +21,8 @@ int main(){
   printf("Ingresar numero de pasos (n)-> ");
   scanf("%d", &n);
   printf("Espere un momento mientras se calcula la solucion... \n");
-  euler(x0, y0, n, h, &Inicio);
-  guardarEnArchivo(Inicio);
+  euler(x0, y0, n, h);
   printf("Ejecucion terminada... \n");
-  liberarMemoria(Inicio);
   plot();
   return 0;
 }
@@ -43,59 +34,26 @@ float f(float x, float y){
   return x + 3*y;
 }
 
-// Funcion que agrega un nodo a la lista dinamica
-void agregarNodo(float x, float y, Nodo** Inicio){
-  Nodo* temp;
-  Nodo* temp2 = (Nodo*)malloc(sizeof(Nodo));
-  temp2->x = x;
-  temp2->y = y;
-  temp2->sig = NULL;
-  if (*Inicio == NULL) {
-    *Inicio = temp2;
-  }else{
-    temp = *Inicio;
-    while (temp->sig != NULL) {
-      temp = temp->sig;
-    }
-    temp->sig = temp2;
-  }
-}
+
 
 // Funcion que resuelve la ecuacion diferencial por el metodo de euler
-void euler(float x0, float y0, int n, float h, Nodo** Inicio){
-  float xaux, yaux;
-  agregarNodo(x0, y0, Inicio);
-  Nodo* ant = *Inicio;
-  for(int i = 0; i < n; i++){
-    yaux = ant->y + h*f(ant->x, ant->y);
-    xaux = ant->x + h;
-    agregarNodo(xaux, yaux, Inicio);
-    ant = ant->sig;
-  }
-}
-
-// Funcion que almacena en un archivo los valores generados
-void guardarEnArchivo(Nodo* Inicio){
-  Nodo* temp = Inicio;
+void euler(float x0, float y0, int n, float h){
   FILE* fp = fopen("solucion.dat", "wt");
-  while (temp != NULL) {
-    fprintf(fp, "%f, %f\n", temp->x, temp->y);
-    temp = temp->sig;
+  float xaux, yaux, anty= y0, antx= x0;
+  fprintf(fp, "%f, %f\n", antx, anty);
+  for(int i = 0; i < n; i++){
+    yaux = anty + h*f(antx, anty);
+    xaux = antx + h;
+    antx = xaux;
+    anty = yaux;
+    fprintf(fp, "%f, %f\n", xaux, yaux);
   }
   fclose(fp);
 }
+
+
 //  plot exp(x)*(-x*exp(-x)-exp(-x)+2), "solucion.dat" with lines
 
-// Funcion que libera la memoria de la computadora
-void liberarMemoria(Nodo* Inicio){
-  Nodo* temp = Inicio;
-  Nodo* ant = Inicio;
-  while (temp != NULL) {
-    temp = temp->sig;
-    free(ant);
-    ant = temp;
-  }
-}
 
 // Función que gráfica el archivo de texto generado
 void plot(){
